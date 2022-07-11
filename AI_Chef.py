eb9afc13b05e2574fedd5afb64ad49b250fa5859
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2022-07-11 11:51:34 -0700

    Update AI_Chef.py

--- AI_Chef.py
+++ AI_Chef.py
@@ -1,2 +1,5 @@
 # Title: AI Chef Search Engine
 # Class: CS 361 - Softwaree Engineering I
+# Author: Mallory Huston
+# Description: An artificially intelligent chef that cooks a recipe you want.
+# Thus programs runs via a search engine on a command prompt interface.
