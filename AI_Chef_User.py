17a97b1e58ce068f2a7e27129c747fc0812989af
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2022-07-31 23:22:11 -0700

    Create AI_Chef_User.py

---
+++ AI_Chef_User.py
@@ -0,0 +1,10 @@
+# Title: AI Chef User Directory
+# Class: CS 361 - Software Engineering I
+# Author: Mallory Huston
+# Description: Defines the User class used in the AI Chef web service.
+
+import json
+from pathlib import Path
+import os
+
+
