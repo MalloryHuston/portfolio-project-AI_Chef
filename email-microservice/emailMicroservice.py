fa99ee9cdea27436004b23ff928bc486a9aa1ee1
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2022-08-05 19:25:36 -0700

    Create emailMicroservice.py

---
+++ email-microservice/emailMicroservice.py
@@ -0,0 +1,122 @@
+import pika,sys,os
+import ssl
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import smtplib
+from email.message import EmailMessage
+
+
+
+def communication():
+    """
+    A function that will constantly run and wait for a message from RabbitMQ communication
+    line. Once message is received, will open up the email GUI.
+    """
+    openEmail = ""
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='email')
+
+    def callback(ch,method,properties,body):
+        openEmail = body
+        if(str(body) == "b'yes'"):
+            email()
+
+
+    channel.basic_consume(queue='email',on_message_callback=callback,auto_ack = True)
+
+    channel.start_consuming()
+
+
+def email():
+    """
+    Email GUI. Will open when message is received.
+    Users must put in message content and subject content for message
+    to be send.
+    """
+    root = Tk()
+    root.title("Email")
+    root.geometry("900x450")
+
+    frame = Frame(root, height = 200, width = 900)
+    frame.pack()
+
+
+    def display_text():
+        """
+        Populates the email message with the contents from the widgets.
+        Message must contain body content and subject content.
+        !!!IMPORTANT!!! s.login can be changed to any email but
+        the password must be the app password for gmail. Not regular
+        gmail password. The second argument in s.sendmail is the email
+        you want the message to go to.
+        """
+
+        bodyString = emailBody.get(1.0, END)
+        if len(bodyString) == 1:
+            messagebox.showerror("Show Error","No Message Inputted")
+
+        elif len(subjectMessage.get()) == 0:
+            messagebox.showerror("Show Error","No Subject Inputted")
+
+        else:
+            msg = EmailMessage()
+            msg.set_content(bodyString)
+            msg['Subject'] = subjectMessage.get()
+            msg['From'] = "heinrija@oregonstate.edu"
+            msg['To'] = "jacob.heinrich09@gmail.com"
+
+            s = smtplib.SMTP('smtp.gmail.com',587)
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            # login for smtplib server (password is gmail app password SEE INFO = https://support.google.com/accounts/answer/185833?hl=en)
+            s.login('CS361SoftwareEngineering@gmail.com','yoevauamcinrcxvs')
+            # First argument is where the email is getting sent from. Second argument is where it is going
+            s.sendmail('CS361SoftwareEngineering','jacob.heinrich09@gmail.com',msg.as_string())
+            s.quit()
+            emailBody.delete(1.0,END)
+            subjectMessage.delete(0, END)
+            messagebox.showinfo("Success","Email Successfully Sent")
+            return
+
+
+
+
+    # Widgets added for email subject and email body
+    emailSubject = Label(frame,text = "Subject")
+    emailSubject.pack()
+
+    bodyTitle = Label(root,text = "Message")
+    bodyTitle.pack()
+
+    subjectMessage = Entry(frame, width = 50)
+    subjectMessage.focus_set()
+    subjectMessage.pack()
+
+    emailBody = Text(root, width = 60, height = 20,bg = "lightgray")
+    emailBody.pack()
+
+
+    button_frame = Frame(root)
+    button_frame.pack()
+
+
+    # will call display text to send email
+    submitButton = Button(
+                root,
+                text="Send Email",
+                command = display_text,
+                fg="black")
+
+    submitButton.pack( side = BOTTOM )
+
+
+    root.mainloop()
+
+
+while True:
+    communication()
