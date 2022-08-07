f9efb93d7a4fba39452a4bad34cef8b9b4de3714
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2022-08-07 14:25:33 -0700

    Update emailMicroservice.py

--- email-microservice/emailMicroservice.py
+++ email-microservice/emailMicroservice.py
@@ -12,7 +12,8 @@ def communication():
     """
     openEmail = ""
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
     channel.queue_declare(queue='email')
@@ -24,7 +25,9 @@ def communication():
 
         print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue='email', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='email',
+                          on_message_callback=callback,
+                          auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
