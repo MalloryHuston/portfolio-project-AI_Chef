536b03f47b0f8a532b611ad5ecf1e2ecf72c108d
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2022-08-06 21:59:10 -0700

    Update send.py

--- email-microservice/send.py
+++ email-microservice/send.py
@@ -8,4 +8,5 @@ channel = connection.channel()
 channel.queue_declare(queue='email')
 
 channel.basic_publish(exchange='', routing_key='email', body='yes')
+print(" [x] Sent email successfully!")
 connection.close()
